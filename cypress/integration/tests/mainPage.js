144d24f15b7a5b4a922671c31e4de2858ca09b98
cbpa <cbpa@vm-cbpa.technodom.kz>
2020-03-07 15:29:18 +0600

    init commit 2

--- cypress/integration/tests/mainPage.js
+++ cypress/integration/tests/mainPage.js
@@ -1,6 +1,7 @@
 describe('check main page', function() {
     it('first', function() {
         cy.visit('/')
-        cy.contains('Приглашаем в путешествия')
+        // cy.contains('ПОДОБРАТЬ ПОХОД ИЛИ АКТИВНЫЙ ТУР')
+        cy.get('nav > :nth-child(1) > .item_1 > :nth-child(1)').trigger('mouseover')
     })
 })
